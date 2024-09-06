80053a6a03320cbe25f4a11864b3d3aa2b03f00e
Mohamed Elgaedi <mohamed.elgaedi@viavisolutions.com>
2024-09-06 04:58:50 +0300

    fix: reset font family to default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Aclonica, Nunito_Sans } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import { Providers } from "../store/provider";
-import "react-toastify/dist/ReactToastify.css";
-
-const aclonica = Aclonica({
-	weight: "400",
-	variable: "--font-aclonica",
-	subsets: ["latin"],
-	display: "swap",
-});
-
-const nunito = Nunito_Sans({
-	weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
-	variable: "--font-nunito-sans",
-	subsets: ["latin"],
-	display: "swap",
-});
-import "@/styles/index.css";
 import Header from "@/components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
+import "react-toastify/dist/ReactToastify.css";
+import "@/styles/index.css";
 
 export const metadata: Metadata = {
 	title: "Comma | Online Courses",
@@ -34,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={`${aclonica.variable} ${nunito.variable}`}>
+		<html lang="en">
 			<body>
 				<Providers>
 					<Header />
