4823c02d210d808c40bdddfd1195629b5d64ae56
Mohamed Elgaedi <mohamed.elgaedi@viavisolutions.com>
2024-09-14 12:15:10 +0300

    chore: refactor home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import CourseList from "@/components/Courses/CourseList/CourseList";
-// import InstructorList from "@/components/Instructor/InstructorList/InstructorList";
+import InstructorList from "@/components/Instructor/InstructorList/InstructorList";
 import ClientsList from "@/features/landingPage/components/ClientsList/ClientsList";
 import StatisticsList from "@/features/landingPage/components/StatisticsList/StatisticsList";
 import Banner from "@/features/landingPage/components/Banner/Banner";
 // import CategoriesList from "@/components/Categories/CategoriesList/CategoriesList";
-import UserReviewList from "@/components/User/UserReviewList/UserReviewList";
-import { Suspense } from "react";
-import Loader from "@/components/ui/Loader/Loader";
+// import UserReviewList from "@/components/User/UserReviewList/UserReviewList";
+// import { Suspense } from "react";
+// import Loader from "@/components/ui/Loader/Loader";
 import "./page.css";
 
 export default async function Home({
@@ -15,38 +15,72 @@ export default async function Home({
 	searchParams?: { category?: string };
 }) {
 	return (
-		<main className="main">
-			<Banner />
-			<h2 className="section-header">
-				Choose category to learn more about the courses in this category
-			</h2>
-			{/* Featured course */}
-			<article className="container courses-collection">
-				<article>
-					<h2 className="courses-header heading-1">Recorded Courses</h2>
-					<CourseList type="recorded" />
-				</article>
-				<article>
-					<h2 className="courses-header heading-1">Interactive Courses</h2>
-					<CourseList type="interactive" />
-				</article>
-				<article>
-					<h2 className="courses-header heading-1">Offline Courses</h2>
-					<CourseList type="offline" />
-				</article>
-			</article>
-
-			<ClientsList />
-			<div className="container">
-				{/* <InstructorList
-					url={`${process.env.NEXT_PUBLIC_API_URL}instructors?_start=0&_limit=4`}
-				/> */}
-				<StatisticsList />
+		<main className="home-page">
+			<div className="home-page__top-section container">
+				<Banner />
+				{/* Categories list */}
+				<section className="home-page__section home-page__categories">
+					<h2 className="home-page__categories-header">
+						Choose category to learn more about the courses in this category
+					</h2>
+				</section>
+				{/* Featured Courses */}
+				<section className="home-page__section home-page__featured-courses">
+					<article>
+						<div className="home-page__section-header">
+							<h2 className="heading-1">Recorded Courses</h2>
+						</div>
+						<CourseList type="recorded" />
+					</article>
+					<article>
+						<div className="home-page__section-header">
+							<h2 className="heading-1">Interactive Courses</h2>
+						</div>
+						<CourseList type="interactive" />
+					</article>
+					<article>
+						<div className="home-page__section-header">
+							<h2 className="heading-1">Offline Courses</h2>
+						</div>
+						<CourseList type="offline" />
+					</article>
+				</section>
+			</div>
+			<div className="home-page__bottom-section-wrapper">
+				<div className="home-page__bottom-section container">
+					{/* Clients list */}
+					<section className="home-page__section home-page__featured-clients">
+						<div className="home-page__section-header">
+							<h2 className="heading-1">Most important clients</h2>
+							<p className="home-page__clients-section-subheader">
+								Trusted by the industry&apos;s best experts
+							</p>
+						</div>
+						<ClientsList />
+					</section>
+					{/* Experts */}
+					<section className="home-page__section home-page__featured-experts">
+						<div className="home-page__section-header">
+							<h2 className="heading-1">Comma Experts</h2>
+						</div>
+						<InstructorList />
+					</section>
+					{/* Statistics */}
+					<section className="home-page__section home-page__stats">
+						<div className="home-page__section-header">
+							<h2 className="heading-1">A platform that delivers results</h2>
+							<p className="home-page__stats-section-subheader">
+								We measure our Success by our driven values
+							</p>
+						</div>
+						<StatisticsList />
+					</section>
+					{/* Students reviews */}
+					{/* <section>
 				<h2>Loved by Our Learners</h2>
-				{/* <CategoriesList /> */}
-				<Suspense fallback={<Loader />} key={JSON.stringify(searchParams)}>
-					<UserReviewList category={searchParams?.category ?? "computer"} />
-				</Suspense>
+				<UserReviewList category={searchParams?.category ?? "computer"} />
+			</section> */}
+				</div>
 			</div>
 		</main>
 	);
